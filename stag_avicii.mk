331092548dfdf1cba0c0927fd6c55e2bda392291
DrtSinX98 <pritish@stag-os.org>
2021-05-11 21:43:57 +0200

    Stagify

--- stag_avicii.mk
+++ stag_avicii.mk
@@ -22,14 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/avicii/device.mk)
 
 # Inherit some common Pixys stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+$(call inherit-product, vendor/stag/main.mk)
 
-# PixysOS Properties
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := true
-
-PRODUCT_NAME := pixys_avicii
+PRODUCT_NAME := stag_avicii
 PRODUCT_DEVICE := avicii
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
